1864e4e68a8b97261a4509ca16f86c5d070c8937
Selamawit <122413283+Selam1431@users.noreply.github.com>
2026-08-15 20:49:37 -0700

    Add: merge-k-sorted-linked-lists - submission-4

---
+++ Data Structures & Algorithms/merge-k-sorted-linked-lists/submission-4.py
@@ -0,0 +1,19 @@
+
+class Solution:    
+    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+       node = []
+
+       for head in lists:
+        while head:
+            node.append(head.val)
+            head= head.next
+
+       node.sort()
+
+       dummy = ListNode(0)
+       cur = dummy
+
+       for value in node:
+            cur.next = ListNode(value)
+            cur = cur.next
+       return dummy.next
